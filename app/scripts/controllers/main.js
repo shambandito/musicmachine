a94f58801d38434497e515bc24c3be27fd982896
shambandito <rubenstoesser@yahoo.co.uk>
2015-04-16 11:50:15 +0200

    cleaned app

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,10 @@
  */
 angular.module('musicmachineApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+
+
+
+
+
   });
