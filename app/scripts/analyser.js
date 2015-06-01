1dc7cbfe2a6bf02153c789190a85ef886771e6a3
Matthias Heieck <matthiasheieck@gmail.com>
2015-06-01 14:50:35 +0200

    fixed performance issues

--- app/scripts/analyser.js
+++ app/scripts/analyser.js
@@ -16,7 +16,7 @@ function updateVisualization () {
 
 	drawBars(array);		
 
-	rafID = window.requestAnimationFrame(updateVisualization);
+	//rafID = window.requestAnimationFrame(updateVisualization);
 }
 
 function initBinCanvas () {
@@ -34,6 +34,10 @@ function initBinCanvas () {
 
 	c.width = dimensions.width;
 	c.height = dimensions.height;
+
+	WIDTH = c.width;
+	HEIGHT = c.height;
+
 	//get context from canvas for drawing
 	ctx = c.getContext("2d");
 
@@ -49,10 +53,6 @@ function initBinCanvas () {
 }
 
 function drawBars (array) {
-
-	WIDTH = canvas.width;
-  	HEIGHT = canvas.height;
-
 	analyser.fftSize = 256;
     var bufferLength = analyser.frequencyBinCount;
     var dataArray = new Uint8Array(bufferLength);
@@ -61,10 +61,9 @@ function drawBars (array) {
 
     function drawBars() {
       drawVisual = requestAnimationFrame(drawBars);
-
       analyser.getByteFrequencyData(dataArray);
 
-      ctx.fillStyle = 'rgb(255, 255, 255)';
+      ctx.fillStyle = 'rgb(238, 238, 238)';
       ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
       var barWidth = (WIDTH / bufferLength) * 2.5;
@@ -74,34 +73,11 @@ function drawBars (array) {
       for(var i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i];
 
-        ctx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-        ctx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+        ctx.fillStyle = 'rgb(63, 81, ' + (barHeight + 80) + ')';
+        ctx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
 
         x += barWidth + 1;
       }
     };
     drawBars();
-}
-
-//calc the color for the spectral based on the value "v"
-function getColor (v) {
-	var maxVolume = 255;
-	//get percentage of the max volume
-	var p = v / maxVolume;
-	var np = null;
-
-	if (p < 0.05) {
-		np = [0,0,0] //black
-	//p is between 0.05 and 0.25
-	} else if (p < 0.25) {
-		np = [parseInt(255 * (1-p)),0,0] //between black and red
-	//p is between 0.25 and 0.75
-	} else if (p < 0.75) {
-		np = [255,parseInt(255 * (1-p)),0];	 //between red and yellow
-	//p is between 0.75 and 1
-	} else {
-		np = [255,255,parseInt(255 * (1-p))]; //between yellow and white
-	}
-
-	return 'rgb('+ (np[0]+","+np[1]+","+np[2]) + ")";
 }
